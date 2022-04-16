2971ed4ff79e4b80c7a2a66c62a0a6b248e8b449
karu samuel ngugi <karusamuelngugi@gmail.com>
2022-04-16 10:01:56 +0300

    Complete assignment

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // code your solution here
+function wrapAdjective(flair="*"){
+
+    return function(text="special"){
+        return "You are "+flair+text+flair+"!"
+    }
+}
+
+function saturdayFun(activity="roller-skate"){
+
+    return "This Saturday, I want to "+activity+"!";
+}
+
+function mondayWork(activity="go to the office"){
+
+    return "This Monday, I will "+activity+".";
+}
